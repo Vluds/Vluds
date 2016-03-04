8f690db04f2b2da6e5dc67a1970439db3e06c76a
tymmesyde <tymmesyde@gmail.com>
2016-03-04 09:30:44 +0100

    update directory const

--- app/core/js/class/app.js
+++ app/core/js/class/app.js
@@ -1,15 +1,17 @@
 var App = {
 
-  rootPath: $(location).attr('href'),
+  rootPath: $(location).attr('pathname'),
+  pagePath: $(location).attr('pathname').split('/')[2],
   viewPath: "app/view/",
   phpClassPath: "app/core/php/class/",
   ajaxContainer: "",
   modelPage: "",
 
   init: function(){
+    this.rootPath = this.rootPath.replace(this.pagePath, '');
     Interface.init();
     Engine.init();
     console.log(this.rootPath);
-    console.log(this.phpClassPath);
+    console.log(this.pagePath);
   }
 }
