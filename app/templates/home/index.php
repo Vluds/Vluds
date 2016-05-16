958b88c8ec50518a5ec8682d9b01c52972e80ecd
TymmeSyde <tymmesyde@gmail.com>
2016-05-16 02:01:29 +0200

    minor changes

--- app/templates/home/index.php
+++ app/templates/home/index.php
@@ -1,1 +1,17 @@
-<h1><?php echo $lang["app-home-title"];?></h1>
+<div id="home-container" class="responsive-container">
+  <div class="icon-large">
+  </div>
+  <div>
+    <div class="avatar-container">
+        <div class="avatar-img"></div>
+    </div>
+
+    <div class="home-separator">
+    </div>
+
+    <div class="avatar-container">
+      <div class="avatar-img"></div>
+      <div class="avatar-add"></div>
+    </div>
+  </div>
+</div>
--- system/core/Ajax.js
+++ system/core/Ajax.js
@@ -12,15 +12,15 @@ var Ajax = {
 
   loadTemplate: function(templateName){
     if(templateName){
-      this.ajaxContainer.stop().animate({'opacity': '0'}, 400);
-        if(this.fileExists(this.templatesPath+templateName+"/style.css")){
+      this.ajaxContainer.stop().animate({'opacity': '0'}, 400).queue(function(){
+        if(Ajax.fileExists(Ajax.templatesPath+templateName+"/style.css")){
           var currentStylesheet = $('link[name='+templateName+']');
           if(currentStylesheet){
             currentStylesheet.remove();
           }
-          $('link.default-stylesheet').after('<link name="'+templateName+'" rel="stylesheet" type="text/css" href="'+this.templatesPath+templateName+"/"+"style.css"+'"></link>');
+          $('link.default-stylesheet').after('<link name="'+templateName+'" rel="stylesheet" type="text/css" href="'+Ajax.templatesPath+templateName+"/"+"style.css"+'"></link>');
         }
-        $.post(this.corePath+"Executor.php", { className: "App", functionName: "loadTemplate", templateName: templateName }, function(data){
+        $.post(Ajax.corePath+"Executor.php", { className: "App", functionName: "loadTemplate", templateName: templateName }, function(data){
           if(!data.error){
             Ajax.ajaxContainer.html(data.reply).stop().animate({'opacity': '1'}, 600);
 
@@ -32,10 +32,12 @@ var Ajax = {
               $('script.default-animation').after('<script name="'+templateName+'" type="text/javascript" src="'+Ajax.templatesPath+templateName+"/script.js"+'"></script>');
             }
           }else{
-            Ajax.ajaxContainer.html(data.error).stop().animate({'opacity': '1'}, 600);
+            Ajax.ajaxContainer.html(data.reply).stop().animate({'opacity': '1'}, 600);
           }
         }, "json");
 
+        $(this).dequeue();
+      });
     }else{
       console.log("empty TEMPLATEnAME");
     }
